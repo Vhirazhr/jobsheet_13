c7264961eb213dc618849bc82a71398fbb9c419f
Vhirazhr <fakhirazahra88@gmail.com>
2023-12-29 15:12:15 +0700

    modify experimen 1 push it!

--- gratitude11.java
+++ gratitude11.java
@@ -3,13 +3,9 @@
  */
 public class gratitude11 {
 
-    public static void sayThankyou(String[] args) {
+    public static void main(String[] args) {
         System.out.println(
                 "Thank you for being my best teacher in this semester, mam. You were inspired me from every angle of my sight.");
     }
 
-    public static void main(String[] args) {
-
-        sayThankyou(args);
-    }
 }
