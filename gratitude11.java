eefd0d4c04b46d46bff085ae3421e0e128886682
Vhirazhr <fakhirazahra88@gmail.com>
2023-12-29 14:37:27 +0700

    push to github

---
+++ gratitude11.java
@@ -0,0 +1,15 @@
+/**
+ * gratitude11
+ */
+public class gratitude11 {
+
+    public static void sayThankyou(String[] args) {
+        System.out.println(
+                "Thank you for being my best teacher in this semester, mam. You were inspired me from every angle of my sight.");
+    }
+
+    public static void main(String[] args) {
+
+        sayThankyou(args);
+    }
+}
